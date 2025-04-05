c5ca850b10cf1b251b03fc749467e698794868d4
rorythomas16 <roryjthomas16@gmail.com>
2025-04-05 15:27:45 +0100

    Updated UI to add plotly html to the input

--- HousePricePredictor_StreamlitUI.py
+++ HousePricePredictor_StreamlitUI.py
@@ -1,6 +1,7 @@
 # imports
 import numpy as np
 import streamlit as st 
+import streamlit.components.v1 as components
 import pickle
 import os
 
@@ -39,14 +40,19 @@ for idx, file in enumerate(list_files_in_directory(model_directory)):
             model[name] = content
         
 # Streamlit UI
-st.title('Machine Learning Model Prediction')
+st.title('Roath (Cardiff) House Price Predictor - by Rory Thomas')
 
+st.header("Data Set used in Training")
+HtmlFile = open("img\\mapped_dataset.html", 'r', encoding='utf-8')
+source_code = HtmlFile.read() 
+components.html(source_code, width=700, height=500, scrolling=False)
+    
+st.header("Data Input")
 # Input fields for the model (you can adjust these based on the features your model requires)
 input_feature_1 = st.number_input('Year', min_value=1970, max_value=2050, value=2025)
 input_feature_2 = st.number_input('Bedrooms', min_value=1, max_value=6, value=1)
 input_feature_3 = st.selectbox('Property Type', ('TERRACED', 'SEMI_DETACHED', 'DETACHED','FLAT'))
 input_feature_4 = st.number_input('Bathrooms', min_value=1, max_value=6, value=1)
-
 
 # Collect all the input features into a numpy array
 input_data = np.array([input_feature_1, input_feature_2, input_feature_3, input_feature_4])
@@ -56,7 +62,8 @@ input_data[2] = propTypeEncoder.transform(input_data[2].reshape(-1,1))[0]
 input_data = scaler.transform(input_data.reshape(1,-1))
 
 # Button to trigger prediction
-if st.button('Make Prediction'):
+_, button_col, _ = st.columns(3) # Centre a button
+if button_col.button('Make Prediction'):
     # Make a prediction using the model
 
     
@@ -68,22 +75,3 @@ if st.button('Make Prediction'):
             prediction = model.predict(input_data)
         print(f"{name}: Prediction = £{prediction[0]:.2f}")
         st.write(f"{name}: Prediction = £{prediction[0]:.2f}")
-
-model = {}
-current_directory = os.getcwd()
-model_directory = os.path.join(current_directory, 'models')
-for idx, file in enumerate(list_files_in_directory(model_directory)):
-    #model.append(pickle.load(os.path.join(model_directory, file)))
-    with open(os.path.join(model_directory,file), 'rb') as pickle_file:
-        content = pickle.load(pickle_file)
-        if 'propTypeEncoder' in file:
-            propTypeEncoder = content
-        elif 'scaler' in file:
-            scaler = content
-        elif 'poly' in file:
-            poly = content
-        else:
-            name = file.split('.')[0].split('_')[-1]
-            print(name)
-            model[name] = content
-    
