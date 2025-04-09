11c00645ad6bd1136e2eb46acc2f17fb35f7a3a2
rorythomas16 <roryjthomas16@gmail.com>
2025-04-09 22:32:25 +0100

    N+9

--- HousePricePredictor_StreamlitUI.py
+++ HousePricePredictor_StreamlitUI.py
@@ -25,7 +25,7 @@ def list_files_in_directory(directory_path):
         print(f"The directory {directory_path} does not exist.")
     
 model = {}
-model_directory = '.'  
+model_directory = '.\\models'  
 for idx, file in enumerate(list_files_in_directory(model_directory)):
     #model.append(pickle.load(os.path.join(model_directory, file)))
     with open(os.path.join(model_directory,file), 'rb') as pickle_file:
