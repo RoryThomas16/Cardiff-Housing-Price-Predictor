7372e21836ee99bd9283e21123829f45dd817225
rorythomas16 <roryjthomas16@gmail.com>
2025-04-09 22:31:09 +0100

    N+7

--- HousePricePredictor_StreamlitUI.py
+++ HousePricePredictor_StreamlitUI.py
@@ -25,11 +25,11 @@ def list_files_in_directory(directory_path):
         print(f"The directory {directory_path} does not exist.")
     
 model = {}
-model_directory = '.\\models'  
+model_directory = '.'  
 for idx, file in enumerate(list_files_in_directory(model_directory)):
     #model.append(pickle.load(os.path.join(model_directory, file)))
     with open(os.path.join(model_directory,file), 'rb') as pickle_file:
-        content = pd.read_pickle(pickle_file)
+        content = pd.load(pickle_file)
         if 'propTypeEncoder' in file:
             propTypeEncoder = content
         elif 'scaler' in file:
