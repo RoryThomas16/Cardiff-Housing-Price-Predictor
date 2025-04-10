9f25d2179198096487d0017c6c13d8e6a01a0c2d
rorythomas16 <roryjthomas16@gmail.com>
2025-04-10 22:01:50 +0100

    N+20

--- HousePricePredictor_StreamlitUI.py
+++ HousePricePredictor_StreamlitUI.py
@@ -25,7 +25,7 @@ def list_files_in_directory(directory_path):
     
 model = {}
 model_directory = os.path.join(os.getcwd(), 'models')  
-#img_directory = os.path.join(os.getcwd(), 'img')
+img_directory = os.path.join(os.getcwd(), 'img')
 for idx, file in enumerate(list_files_in_directory(model_directory)):
     #model.append(pickle.load(os.path.join(model_directory, file)))
     with open(os.path.join(model_directory,file), 'rb') as pickle_file:
