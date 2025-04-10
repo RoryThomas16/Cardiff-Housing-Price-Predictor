2ef34e33fd6889857d7f499a2543588448ea4467
rorythomas16 <roryjthomas16@gmail.com>
2025-04-10 22:13:54 +0100

    Okay, I figured it out. I hadn't clocked the changes I pushed weren't immediately accessible by streamlit. So I had to wait, but because I never I kept overwriting the functions that worked.

--- HousePricePredictor_StreamlitUI.py
+++ HousePricePredictor_StreamlitUI.py
@@ -29,7 +29,6 @@ img_directory = os.path.join(os.getcwd(), 'img')
 for idx, file in enumerate(list_files_in_directory(model_directory)):
     #model.append(pickle.load(os.path.join(model_directory, file)))
     with open(os.path.join(model_directory,file), 'rb') as pickle_file:
-        st.write(pickle_file)
         content = pickle.load(pickle_file)
         if 'propTypeEncoder' in file:
             propTypeEncoder = content
